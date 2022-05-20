5c20061c1f6321a186d0e289d6af14ae0363a4b9
Colin Francis <colinfrancis@Colins-MacBook-Pro.local>
2022-05-20 00:36:21 -0400

    Updated all chars to strings to allow for the same type in priority queue and to allow for easy type cast from string to int

--- main.cpp
+++ main.cpp
@@ -2,19 +2,27 @@
 // Description: An implementation of Dijkstra's Algorithm in C++
 #include <iostream>
 #include <map>
+#include <queue>
+#include <string>
 using namespace std;
 
 
 class Dijkstra {
     private:
-        map<char, map<char, int>> graph;
-        map<char, char> pathTable;
-        map<char, int> distanceTable;
+        map<string, map<string, int>> graph;
+        map<string, string> pathTable;
+        map<string, int> distanceTable;
         map<char, char> routingTable;
-        char source;
+        string source;
+
+        struct comparitor {
+            bool operator() (vector<string>& a, vector<string>& b) {
+                return stoi(a[0]) > stoi(b[0]);
+            }
+        };
 
         void initDistanceTable(void) {
-            map<char, map<char, int>>::iterator graphIter = this->graph.begin();
+            map<string, map<string, int>>::iterator graphIter = this->graph.begin();
 
             while (graphIter != this->graph.end()) {
                 if (graphIter->first == this->source) {
@@ -26,7 +34,7 @@ class Dijkstra {
                 graphIter = next(graphIter, 1);
             }
 
-            map<char, int>::iterator linkIter = this->graph.find(this->source)->second.begin();
+            map<string, int>::iterator linkIter = this->graph.find(this->source)->second.begin();
             
             while (linkIter != this->graph.find(this->source)->second.end()) {
                 this->distanceTable.find(linkIter->first)->second = linkIter->second;
@@ -35,24 +43,36 @@ class Dijkstra {
         }
 
         void initPathTable(void) {
-            
+            map<string, map<string, int>>::iterator graphIter = this->graph.begin();
+
+            while (graphIter != this->graph.end()) {
+                this->pathTable.insert({graphIter->first, ""});
+                graphIter = next(graphIter, 1);
+            }
+
+            map<string, int>::iterator linkIter = this->graph.find(this->source)->second.begin();
+
+            while (linkIter != this->graph.find(this->source)->second.end()) {
+                this->pathTable.find(linkIter->first)->second = this->source;
+                linkIter = next(linkIter, 1);
+            }
         }
     
     public:
         // Constructor for Dijkstra class
-        Dijkstra(map<char, map<char, int>> graph, char source) {
+        Dijkstra(map<string, map<string, int>> graph, string source) {
             this->graph = graph;
             this->source = source;
         }
 
         // Displays graph of network for class instance
         void getGraph(void) {
-            map<char, map<char, int>>::iterator graphIter = this->graph.begin();
+            map<string, map<string, int>>::iterator graphIter = this->graph.begin();
 
-            cout << "\nGraph of Network: " << endl;
+            cout << "Graph of Network: " << endl;
             while (graphIter != this->graph.end()) {
                 cout << "\t" << graphIter->first << ": {";
-                map<char, int>::iterator linkIter = graphIter->second.begin();
+                map<string, int>::iterator linkIter = graphIter->second.begin();
                 while (linkIter != graphIter->second.end()) {
                     cout << "{" << linkIter->first << ", " << linkIter->second << "}";
                     if (next(linkIter, 1) != graphIter->second.end()) {
@@ -67,17 +87,30 @@ class Dijkstra {
         }
 
         void getPathTable(void) {
+            map<string, string>::iterator pathIter = this->pathTable.begin();
 
+            cout << "Current Node: Previous Node" << endl;
+            while (pathIter != this->pathTable.end()) {
+                cout << "\t" << pathIter->first << ": ";
+                if (pathIter->second == "") {
+                    cout << "*" << endl;
+                } else {
+                    cout << pathIter->second << endl;
+                }
+                pathIter = next(pathIter, 1);
+            }
+            cout << endl;
         }
 
         void getDistanceTable(void) {
-            map<char, int>::iterator distIter = this->distanceTable.begin();
+            map<string, int>::iterator distIter = this->distanceTable.begin();
 
-            cout << "Distance Table From " << this->source << " To Other Nodes:" << endl;
+            cout << "Distance From Node " << this->source << " To Other Nodes:" << endl;
             while (distIter != this->distanceTable.end()) {
                 cout << "\t" << distIter->first << ": " << distIter->second << endl;
                 distIter = next(distIter, 1);
             }
+            cout << endl;
         }
 
         void getRoutingTable(void) {
@@ -85,24 +118,46 @@ class Dijkstra {
         }
 
         void dijkstra(void) {
+            map<string, bool> visited;
+            priority_queue<vector<string>, vector<vector<string>>, comparitor> pq;
             this->initDistanceTable();
+            this->initPathTable();
+
+            this->getGraph();
             this->getDistanceTable();
+            this->getPathTable();
+
+            visited.insert({this->source, true});
+            map<string, int>::iterator linkIter = this->graph.find(this->source)->second.begin();
+            while (linkIter != this->graph.find(this->source)->second.end()) {
+                pq.push({to_string(linkIter->second), linkIter->first});
+                linkIter = next(linkIter, 1);
+            }
+
+            while (!pq.empty()) {
+                vector<string> front = pq.top();
+                pq.pop();
+
+                int distance = stoi(front[0]);
+                string node = front[1];
+
+                cout << "Distance: " << distance << ", " << "Node: " << node << endl;
+            }
         }
 };
 
 int main(int argc, char* argv[]) {
-    map<char, map<char, int>> network = {
-        {'A', map<char, int>{{'B', 7}, {'E', 9}}},
-        {'B', map<char, int>{{'A', 7}, {'C', 3}, {'E', 6}}},
-        {'C', map<char, int>{{'B', 3}, {'D', 11}, {'F', 2}}},
-        {'D', map<char, int>{{'C', 11}, {'G', 3}}},
-        {'E', map<char, int>{{'A', 9}, {'B', 6}}},
-        {'F', map<char, int>{{'B', 8}, {'C', 2}, {'G', 5}}},
-        {'G', map<char, int>{{'D', 3}, {'F', 5}}}
+    map<string, map<string, int>> network = {
+        {"A", map<string, int>{{"B", 7}, {"E", 9}}},
+        {"B", map<string, int>{{"A", 7}, {"C", 3}, {"E", 6}}},
+        {"C", map<string, int>{{"B", 3}, {"D", 11}, {"F", 2}}},
+        {"D", map<string, int>{{"C", 11}, {"G", 3}}},
+        {"E", map<string, int>{{"A", 9}, {"B", 6}}},
+        {"F", map<string, int>{{"B", 8}, {"C", 2}, {"G", 5}}},
+        {"G", map<string, int>{{"D", 3}, {"F", 5}}}
     };
 
-    Dijkstra d = Dijkstra(network, 'D');
-    d.getGraph();
+    Dijkstra d = Dijkstra(network, "D");
     d.dijkstra();
 
     return 0;
